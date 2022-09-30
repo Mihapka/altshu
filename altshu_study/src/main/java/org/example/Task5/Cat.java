50e8940b0b4e87556bddbd7a9c401fb9debf1851
Андрей Александрович Михайлов <mikhaylov.andrey.a@gmail.com>
2022-09-30 10:47:56 +0300

    Task5

---
+++ altshu_study/src/main/java/org/example/Task5/Cat.java
@@ -0,0 +1,8 @@
+package org.example.Task5;
+
+public class Cat extends Animal{
+    @Override
+    public void tellHowAreYouMoving() {
+        System.out.println("я кошла, я бегаю за мышками");
+    }
+}
---
+++ altshu_study/src/main/java/org/example/Task5/Snake.java
@@ -0,0 +1,8 @@
+package org.example.Task5;
+
+public class Snake extends Animal{
+    @Override
+    public void tellHowAreYouMoving() {
+        System.out.println("я змея, рожденный ползать бегать не может");
+    }
+}
